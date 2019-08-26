938037e1b532b8e958fb33402a6209fd1561ec09
Millijava <milenashikor@yahoo.com>
2019-08-26 15:49:12 -0400

    message

---
+++ src/main/java/springboot_103/demo/HomeController.java
@@ -0,0 +1,13 @@
+package springboot_103.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homepage(){
+        return "hometemplate";
+    }
+}
+
